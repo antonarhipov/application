239ab8c60a0830ae57224162b79857285904e6fa
Anton Arhipov <ant.arhipov@gmail.com>
2018-09-18 14:34:56 +0300

    branch

--- src/main/java/org/arhan/program/Handler.java
+++ src/main/java/org/arhan/program/Handler.java
@@ -1,13 +1,17 @@
 package org.arhan.program;
 
+import org.arhan.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Handler {
 
+  Component component = new Component();
+
   @GetMapping(path = "/")
   public String hello() {
-    return "hello!!!";
+    component.foo();
+    return "hello";
   }
 }
